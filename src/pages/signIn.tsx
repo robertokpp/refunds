8b805610118d0bdcc0ad263682529cd5303d5bba
Roberto Kennedy <roberto.kpp@gmail.com>
2026-04-25 19:08:24 -0500

    adiciona componente Button e implementa função de envio no formulário de login

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,6 +1,11 @@
+import { Button } from "../components/Button";
 import { Input } from "../components/input";
 
 export function SignIn() {
+
+  function onSubmit(e){
+    alert("enviado")
+  }
   return (
     <form className="w-full flex flex-col gap-4">
       <Input
@@ -10,6 +15,8 @@ export function SignIn() {
         placeholder="seu@email.com"
       />
       <Input required legend="Senha" type="password" placeholder="123456" />
+
+      <Button>Entra</Button>
     </form>
   );
 }
