2235650a2aae6b407201cccc77e49fa306432a00
Roberto Kennedy <roberto.kpp@gmail.com>
2026-05-06 21:14:16 -0500

    adiciona componente Loading e implementa lógica de carregamento nas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,33 @@ import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
+import { Loading } from "../components/Loading";
 
+const isLoading = false;
+
+const session = {
+  user: {
+    role: "employee",
+  },
+};
 export function Routes() {
+  function Route() {
+    switch (session.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
